bfb9bfccbb5c026aed8cd55260edfd370879e804
erikachyn <163372361+erikachyn@users.noreply.github.com>
2025-02-11 11:32:51 +0700

    new proje

---
+++ resources/views/siswa.blade.php
@@ -0,0 +1,39 @@
+@extends('partials.main')
+ @section('content')
+<h1>Data Siswa</h1>
+<a href="{{Route('siswa.add')}}"class="btn btn-info">Tambah Data</a>
+
+
+
+<table class="table table-striped">
+
+    <tr class="table table-dark" >
+        <th>No</th>
+        <th>Nama</th>
+        <th>Alamat</th>
+        <th>Jenis Kelamin</th>
+        <th>Menu</th>
+    </tr>
+
+
+    @foreach ($siswa as $item)
+    <tr>
+        <td>{{$loop->iteration}}</td>
+        <td>{{$item['nama']}}</td>
+        <td>{{$item['alamat']}}</td>
+        <td>{{$item['jenis_kelamin']}}</td>
+        <td>
+        <form action="{{Route('siswa.delete',$item['id'])}}" method='post'>
+            @method('DELETE')
+            @csrf
+           <button class="btn btn-danger" type="submit">HAPUS</button>
+        </form>
+          <a class="btn btn-primary" href="{{Route('siswa.edit',$item['id'])}}">EDIT</a>
+
+    </td>
+    </tr>
+    @endforeach
+
+</table>
+
+@endsection
